128e72bc38f46155dacae8bc0a6c3d1082053fa0
Zhangsan <zhangsan@qq.com>
2021-01-31 22:21:01 +0800

    修改了git-test中的内容

--- git-Test/src/Test.java
+++ git-Test/src/Test.java
@@ -2,4 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello,git project");
     }
+    public  void hello(){
+        System.out.println("hahahaha");
+    }
 }
